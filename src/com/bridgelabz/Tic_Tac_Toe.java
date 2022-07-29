2986b9b92be8009247823bfb213441c70fbdee83
champ09D <darshanmhatre709@gmail.com>
2022-07-29 13:11:47 +0530

    Added : Desired Location To Move Case File Add

--- src/com/bridgelabz/Tic_Tac_Toe.java
+++ src/com/bridgelabz/Tic_Tac_Toe.java
@@ -25,31 +25,59 @@ public class Tic_Tac_Toe
         System.out.println(board[7]+"|"+board[8]+"|"+board[9]);
     }
     public static void MakeChoice()
+
     {
         Scanner s=new Scanner(System.in);
         System.out.println("Enter the Choice of symbol as X or O :" );
         char userSelect=s.next().charAt(0);
+
         if (userSelect == 'X' || userSelect == 'x' )
         {
             computerChoice='O';
             playerChoice ='X';
-        }
-        else if(userSelect == 'O' || userSelect == 'o' )
+        }else if(userSelect == 'O' || userSelect == 'o' )
         {
             computerChoice='X';
             playerChoice ='O';
-        }
-        else
+        }else
         {
             System.out.println("Invalid Selection");
         }
+
     }
-    public static void main(String[] args)
+    public static void makeMove(char[] board, char playerChoice)
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter location between 1 to 9" );
+        int location=s.nextInt();
+        if (0 < location && location < 10 )
+        {
+            if (board[location] == ' ')
+            {
+                board[location]=playerChoice;
+            }else
+            {
+                System.out.println("Invalid Move");
+                DisplayBoard(board);
+                makeMove(board, playerChoice);
+            }
+        }
+        else
+        {
+            System.out.println("Invalid Location: Try Again");
+            DisplayBoard(board);
+            makeMove(board, playerChoice);
+        }
+    }
+
+    public static void main(String[] darsh)
     {
         System.out.println("--------Welcome To TicTacToe Game----------");
         char[] board =CreateBoard();
         MakeChoice();
         System.out.println("Player selection is:"+playerChoice+"\n"+"Computer Selection is:"+computerChoice);
+        DisplayBoard(board);
+        makeMove(board, playerChoice);
         DisplayBoard(board);
     }
 }
