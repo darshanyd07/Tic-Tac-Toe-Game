2f698f323e62cae3c627faa7a3b46ba756758617
champ09D <darshanmhatre709@gmail.com>
2022-07-29 13:21:04 +0530

    Added : Free Space Before Making Desired Move Case Add

--- src/com/bridgelabz/Tic_Tac_Toe.java
+++ src/com/bridgelabz/Tic_Tac_Toe.java
@@ -6,6 +6,7 @@ public class Tic_Tac_Toe
     static char playerChoice=' ';
     static char computerChoice =' ';
 
+
     public static char[] CreateBoard()
     {
         char[] board=new char[10];
@@ -25,12 +26,12 @@ public class Tic_Tac_Toe
         System.out.println(board[7]+"|"+board[8]+"|"+board[9]);
     }
     public static void MakeChoice()
-
     {
         Scanner s=new Scanner(System.in);
         System.out.println("Enter the Choice of symbol as X or O :" );
         char userSelect=s.next().charAt(0);
-
+        //String computerChoice ="";
+        //String userChoice="";
         if (userSelect == 'X' || userSelect == 'x' )
         {
             computerChoice='O';
@@ -39,38 +40,27 @@ public class Tic_Tac_Toe
         {
             computerChoice='X';
             playerChoice ='O';
-        }else
-        {
+        }else{
             System.out.println("Invalid Selection");
         }
 
     }
-    public static void makeMove(char[] board, char playerChoice)
-    {
+    public static void makeMove(char[] board, char playerChoice){
         Scanner s=new Scanner(System.in);
         System.out.println("Enter location between 1 to 9" );
         int location=s.nextInt();
-        if (0 < location && location < 10 )
-        {
-            if (board[location] == ' ')
-            {
+        if (0 < location && location < 10 ) {
+            if (board[location] == ' ') {
                 board[location]=playerChoice;
-            }else
-            {
-                System.out.println("Invalid Move");
-                DisplayBoard(board);
-                makeMove(board, playerChoice);
             }
-        }
-        else
-        {
+        }else {
             System.out.println("Invalid Location: Try Again");
             DisplayBoard(board);
             makeMove(board, playerChoice);
         }
     }
 
-    public static void main(String[] darsh)
+    public static void main(String[] args)
     {
         System.out.println("--------Welcome To TicTacToe Game----------");
         char[] board =CreateBoard();
