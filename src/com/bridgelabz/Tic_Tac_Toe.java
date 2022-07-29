cd2e17195e1893bf549fc2b70d10d934d2204ecf
champ09D <darshanmhatre709@gmail.com>
2022-07-29 13:04:40 +0530

    Added : Display Board Case File Add

--- src/com/bridgelabz/Tic_Tac_Toe.java
+++ src/com/bridgelabz/Tic_Tac_Toe.java
@@ -1,53 +1,55 @@
 package com.bridgelabz;
-
 import java.util.Scanner;
 
 public class Tic_Tac_Toe
 {
-        public static char[] CreateBoard()
-        {
-            char[] board=new char[10];
+    static char playerChoice=' ';
+    static char computerChoice =' ';
 
-            for(int i=1; i<board.length;i++)
-            {
-                board[i]=' ';
-            }
-            return board;
-        }
-        public static void DisplayBoard(char[] board)
+    public static char[] CreateBoard()
+    {
+        char[] board=new char[10];
+
+        for(int i=1; i<board.length;i++)
         {
-            System.out.println(board[1]+"|"+board[2]+"|"+board[3]);
-            System.out.println("-----");
-            System.out.println(board[4]+"|"+board[5]+"|"+board[6]);
-            System.out.println("-----");
-            System.out.println(board[7]+"|"+board[8]+"|"+board[9]);
+            board[i]=' ';
         }
-        public static void MakeChoice()
+        return board;
+    }
+    public static void DisplayBoard(char[] board)
+    {
+        System.out.println(board[1]+"|"+board[2]+"|"+board[3]);
+        System.out.println("-----");
+        System.out.println(board[4]+"|"+board[5]+"|"+board[6]);
+        System.out.println("-----");
+        System.out.println(board[7]+"|"+board[8]+"|"+board[9]);
+    }
+    public static void MakeChoice()
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the Choice of symbol as X or O :" );
+        char userSelect=s.next().charAt(0);
+        if (userSelect == 'X' || userSelect == 'x' )
         {
-            Scanner s=new Scanner(System.in);
-            System.out.println("Enter the Choice of symbol as X or O :" );
-            char userSelect=s.next().charAt(0);
-            char computerChoice =' ';
-            char userChoice=' ';
-            if (userSelect == 'X' || userSelect == 'x' )
-            {
-                computerChoice='O';
-                userChoice ='X';
-            }
-            else if(userSelect == 'O' || userSelect == 'o' )
-            {
-                computerChoice='X';
-                userChoice ='O';
-            }else
-            {
-                System.out.println("Invalid Selection");
-            }
-            System.out.println("You Select :- "+userChoice+ " Computer Get :-"+computerChoice);
+            computerChoice='O';
+            playerChoice ='X';
         }
-            public static void main(String[] args)
-            {
-            char[] board =CreateBoard();
-            MakeChoice();
-        //DisplayBoard(board);
-            }
+        else if(userSelect == 'O' || userSelect == 'o' )
+        {
+            computerChoice='X';
+            playerChoice ='O';
+        }
+        else
+        {
+            System.out.println("Invalid Selection");
+        }
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("--------Welcome To TicTacToe Game----------");
+        char[] board =CreateBoard();
+        MakeChoice();
+        System.out.println("Player selection is:"+playerChoice+"\n"+"Computer Selection is:"+computerChoice);
+        DisplayBoard(board);
+    }
 }
