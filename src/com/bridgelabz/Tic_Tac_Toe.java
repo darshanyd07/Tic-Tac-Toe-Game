f5d3db58092d5b71da9bf9223f094ebf7b40fe86
champ09D <darshanmhatre709@gmail.com>
2022-07-29 17:54:25 +0530

    Added : Blocking Opponent Move Case File Add

--- src/com/bridgelabz/Tic_Tac_Toe.java
+++ src/com/bridgelabz/Tic_Tac_Toe.java
@@ -2,181 +2,274 @@ package com.bridgelabz;
 import java.util.Scanner;
 public class Tic_Tac_Toe
 {
-    static int gameStatus;
-    static int location;
-    static char playerChoice=' ';
-    static char computerChoice =' ';
-    static int count=0;
-    static int currentPlayer;
-    static char[] board =CreateBoard();
-
-    public static char[] CreateBoard()
+    public char computerChoice;
+    public char userChoice;
+    public int counter = 0;
+    public char[] board;
+    //    create board
+    public char[] createBoard()
     {
-        char[] board=new char[10];
-
-        for(int i=1; i<board.length;i++)
+        board = new char[10];
+        for (int i = 1; i < board.length; i++)
         {
-            board[i]=' ';
+            board[i] = ' ';
         }
         return board;
     }
-
-    public static void DisplayBoard(char[] board)
+    //creating make choice for x or o
+    public void makeChoice()
     {
-        System.out.println(board[1]+"|"+board[2]+"|"+board[3]);
-        System.out.println("-----");
-        System.out.println(board[4]+"|"+board[5]+"|"+board[6]);
-        System.out.println("-----");
-        System.out.println(board[7]+"|"+board[8]+"|"+board[9]);
-    }
-    public static void MakeChoice()
-    {
-        if (currentPlayer == 0)
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter what you want X or O ");
+        char player = s.next().charAt(0);
+        char computer = ' ';
+        if (player == 'X' || player == 'x')
         {
-            int computerSelect=(int) Math.floor((Math.random()*10)%2);
-            if (computerSelect == 0)
+            computerChoice = 'O';
+            userChoice = 'X';
+        } else if (player == 'O' || player == 'o')
+        {
+            computerChoice = 'X';
+            userChoice = 'O';
+        } else {
+            System.out.println("invalid");
+        }
+        System.out.println("player is " + userChoice + " " + "computer is " + computerChoice);
+    }
+    //display board
+    public static void displayBoard(char[] board)
+    {
+        System.out.println(board[1] + "|" + board[2] + "|" + board[3]);
+        System.out.println("------");
+        System.out.println(board[4] + "|" + board[5] + "|" + board[6]);
+        System.out.println("------");
+        System.out.println(board[7] + "|" + board[8] + "|" + board[9]);
+    }
+    //if location is empty and added player value
+    public boolean validateMove(int location)
+    {
+        if (0 < location && location < 10)
+        {
+            if (board[location] != ' ') return false;
+            return true;
+        }
+        return false;
+    }
+    //make move for user only
+    public void makeUserMove()
+    {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("enter a location number ");
+        int location = obj.nextInt();
+        if (validateMove(location))
+        {
+            board[location] = userChoice;
+            counter++;
+            checkGameStatus("User");
+        } else
+        {
+            System.out.println("Sorry you have selected the wrong position try again");
+            if (counter <= 9) makeUserMove();
+            else return;
+        }
+    }
+    //computer move
+    public void makeComputerMove()
+    {
+        int move = (int) Math.floor(Math.random() * 10) % 9 + 1;
+        System.out.println("Wait its computers turn now");
+        int bestmove = computerWinMove();
+        if (bestmove != 0)
+        {
+            board[bestmove] = computerChoice;
+            System.out.println(bestmove);
+            counter++;
+            checkGameStatus("Computer");
+            return;
+        }
+        bestmove = computerBlockMove();
+        if (bestmove != 0)
+        {
+            board[bestmove] = computerChoice;
+            System.out.println(bestmove);
+            counter++;
+            checkGameStatus("Computer");
+            return;
+        }
+        if (bestmove == 0)
+        {
+            if (validateMove(move))
             {
-                computerChoice='O';
-                playerChoice ='X';
-            }else {
-                computerChoice='X';
-                playerChoice ='O';
-            }
-        }else{
-            Scanner s=new Scanner(System.in);
-            System.out.println("Enter the Choice of symbol as X or O :" );
-            char userSelect=s.next().charAt(0);
-            if (userSelect == 'X' || userSelect == 'x' )
-            {
-                computerChoice='O';
-                playerChoice ='X';
-            }else if(userSelect == 'O' || userSelect == 'o' )
-            {
-                computerChoice='X';
-                playerChoice ='O';
-            }else{
-                System.out.println("Invalid Selection");
+                board[move] = computerChoice;
+                System.out.println(move);
+                counter++;
+                checkGameStatus("Computer");
+            } else {
+                if (counter <= 9) makeComputerMove();
+                else return;
             }
         }
     }
-
-    public static void makeMove(char[] board, char playerChoice,char computerChoice)
+    //who play first
+    public static boolean tossToBegin()
     {
-        if(currentPlayer==1)
+        int toss = (int) Math.floor(Math.random() * 10) % 2;
+        if (toss == 0) {
+            System.out.println("Player its your turn to play");
+            return false;
+        } else {
+            System.out.println("computer it's your turn to play");
+            return true;
+        }
+    }
+    //check condition if win
+    public boolean allEqual(int a, int b, int c)
+    {
+        return (a == c) && (b == c);
+    }
+    //check condition to win
+    public boolean twoEqual(char a,char b)
+    {
+        return (a == b) && a == computerChoice;
+    }
+    //check if computer can make best move
+    public int computerWinMove()
+    {
+        for (int i = 1; i < 10; i = i + 3)
         {
-            Scanner s=new Scanner(System.in);
-            System.out.println("Enter location between 1 to 9" );
-            location=s.nextInt();
-            if (0 < location && location < 10 )
+            if (twoEqual(board[i] , board[i + 1]) && board[i + 2] == ' ')
             {
-                if (board[location] == ' ')
+                return i + 2;
+            }
+        }
+        for (int i = 1; i < 10; i = i + 3) {
+            if (twoEqual(board[i], board[i + 2]) && board[i + 1] == ' ')
+            {
+                return i + 1;
+            }
+        }
+        for (int i = 1; i < 10; i = i + 3)
+        {
+            if (twoEqual(board[i + 1] , board[i + 2]) && board[i] == ' ')
+            {
+                return i;
+            }
+        }
+        for (int i = 1; i < 3; i = i + 1)
+        {
+            if (twoEqual(board[i] , board[i + 3]) && board[i + 6] == ' ')
+            {
+                return i + 6;
+            }
+        }
+        for (int i = 1; i < 3; i = i + 1) {
+            if (twoEqual(board[i] , board[i + 6]) && board[i + 3] == ' ')
+            {
+                return i + 3;
+            }
+        }
+        for (int i = 1; i < 3; i = i + 1) {
+            if (twoEqual(board[i + 3] , board[i + 6]) && board[i] == ' ')
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+    public boolean checkToBlock(char a,char b)
+    {
+        return (a == b) && a == userChoice;
+    }
+    //check if computer can block user
+    public int computerBlockMove()
+    {
+        for (int i = 1; i < 10; i = i + 3)
+        {
+            if (checkToBlock(board[i] , board[i + 1]) && board[i + 2] == ' ')
+            {
+                return i + 2;
+            }
+        }
+        for (int i = 1; i < 10; i = i + 3)
+        {
+            if (checkToBlock(board[i], board[i + 2]) && board[i + 1] == ' ')
+            {
+                return i + 1;
+            }
+        }
+        for (int i = 1; i < 10; i = i + 3)
+        {
+            if (checkToBlock(board[i + 1] , board[i + 2]) && board[i] == ' ')
+            {
+                return i;
+            }
+        }
+        for (int i = 1; i < 3; i = i + 1) {
+            if (checkToBlock(board[i] , board[i + 3]) && board[i + 6] == ' ')
+            {
+                return i + 6;
+            }
+        }
+        for (int i = 1; i < 3; i = i + 1) {
+            if (checkToBlock(board[i] , board[i + 6]) && board[i + 3] == ' ')
+            {
+                return i + 3;
+            }
+        }
+        for (int i = 1; i < 3; i = i + 1) {
+            if (checkToBlock(board[i + 3] , board[i + 6]) && board[i] == ' ')
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    public void checkGameStatus(String key)
+    {
+        displayBoard(board);
+        // System.out.println(counter);
+        if (counter <= 9) {
+            for (int i = 1; i < 10; i = i + 3)
+            {
+                if (allEqual(board[i], board[i + 1], board[i + 2]) && board[i] != ' ')
                 {
-                    board[location]=playerChoice;
+                    System.out.println(key + " has won the game");
+                    return;
                 }
-            }else
-            {
-                System.out.println("Invalid Location: Try Again");
-                DisplayBoard(board);
-                makeMove(board, playerChoice,computerChoice);
             }
-        }else{
-            location=(int) Math.floor((Math.random()*9)+1);
-            if (0 < location && location < 10 )
+            for (int i = 1; i <= 3; i = i + 1)
             {
-                if (board[location] == ' ')
+                if (allEqual(board[i], board[i + 3], board[i + 6]) && board[i] != ' ')
                 {
-                    board[location]=computerChoice;
+                    System.out.println(key + " has won the game");
+                    return;
                 }
-            }else{
-                System.out.println("Invalid Location: Try Again");
-                DisplayBoard(board);
-                makeMove(board, playerChoice,computerChoice);
             }
-        }
-
-        currentPlayer=currentPlayer+1;
-    }
-
-    public static int Toss()
-    {
-        int flipToss=(int) Math.floor((Math.random()*10)%2);
-        if (flipToss == 0)
-        {
-            System.out.println("Player chance to play first");
-            currentPlayer=1;
-        }else
-
-        {
-            System.out.println("Computer chance to play first");
-            currentPlayer=0;
-        }
-        return flipToss;
-    }
-
-    public static void 	DetermineCondition(char playerChoice)
-    {
-        count++;
-        if (board[1] == board[2] && board[2] == board[3] && board[3]==playerChoice)
-        {
-            gameStatus=1;
-        }
-        else if (board[4] == board[5] && board[5] == board[6] && board[6]==playerChoice)
-        {
-            gameStatus=1;
-        }
-
-        else if (board[7] == board[8] && board[8] == board[9]&& board[9]==playerChoice)
-        {
-            gameStatus=1;
-        }
-        else if (board[1]==board[4] && board[4] == board [7]&& board[7]==playerChoice)
-        {
-            gameStatus=1;
-        }
-        else if (board[2]==board[5] && board[5]==board[8]&& board[8]==playerChoice)
-        {
-            gameStatus=1;
-        }
-        else if (board[3]==board[6] && board[6]==board[9]&& board[9]==playerChoice)
-        {
-            gameStatus=1;
-        }
-        else if (board[1]==board[5] && board[5]==board[9]&& board[9]==playerChoice)
-        {
-            gameStatus=1;
-        }
-        else if (board[3]==board[5] && board[5]==board[7]&& board[7]==playerChoice)
-        {
-            gameStatus=1;
-        }
-        else
-        {
-            gameStatus=0;
-        }
-
-        if(gameStatus==1)
-        {
-            System.out.println("Player Wins");
-        }
-        else
-        {
-            System.out.println("Turn Changed");
+            if (allEqual(board[1], board[5], board[9]) && board[1] != ' ')
+            {
+                System.out.println(key + " has won the game");
+                return;
+            }
+            if (allEqual(board[3], board[5], board[7]) && board[3] != ' ')
+            {
+                System.out.println(key + " has won the game");
+                return;
+            }
+            if (key.equals("Computer") && counter < 9) makeUserMove();
+            else if (key.equals("User") && counter < 9) makeComputerMove();
+            else System.out.println("It's a tie");
         }
     }
-    public static void main(String[] args)
+
+    public static void main(String[] darsh)
     {
-
-        System.out.println("--------Welcome To TicTacToe Game----------");
-        Toss();
-        MakeChoice();
-        System.out.println("Player selection is:"+playerChoice+"\n"+"Computer Selection is:"+computerChoice);
-        DisplayBoard(board);
-        makeMove(board, playerChoice,computerChoice);
-        DisplayBoard(board);
-        DetermineCondition(playerChoice);
-        if(count==9){
-            System.out.println("Game is Tie");}
-
-        currentPlayer=currentPlayer%2;
+        int gamestatus = 0;
+        Tic_Tac_Toe game = new Tic_Tac_Toe();
+        System.out.println("--------Welcome To Tic Tac Toe Game--------");
+        char[] board = game.createBoard();
+        game.makeChoice();
+        if (tossToBegin()) game.makeComputerMove();
+        else game.makeUserMove();
     }
 }
